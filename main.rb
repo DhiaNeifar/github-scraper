0d517a7b850e3698050889d6f0ecd2d9a95dfe67
dhianeifar <dhia.neifar@gmail.com>
2025-06-25 15:56:52 -0400

    First Commit! Created a main, started argparser.

---
+++ main.rb
@@ -0,0 +1,31 @@
+require 'optparse'
+
+def main(options)
+  puts "Scraping organization: #{options[:org]}"
+  # Your scraping logic goes here
+end
+
+if __FILE__ == $0
+  options = { org: "vercel" }
+
+  parser = OptionParser.new do |opts|
+    opts.banner = "\nUsage: scraper.rb [options]"
+    opts.separator ""
+    opts.separator "This tool scrapes public repositories for a given GitHub organization."
+    opts.separator "You can specify the organization name, or use the default: 'vercel'."
+    opts.separator ""
+    opts.separator "Options:"
+
+    opts.on("--org=ORG", "GitHub organization (default: vercel)") do |org|
+      options[:org] = org
+    end
+
+    opts.on("-h", "--help", "Prints this help message") do
+      puts opts
+      exit
+    end
+  end
+
+  parser.parse!
+  main(options)
+end
